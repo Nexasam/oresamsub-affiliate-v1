5ca06d10a72034980cfb0bb2c86a49fea74f755a
Sammiee4real <adebsholey4real@gmail.com>
2026-05-19 03:19:18 +0100

    fix issue

--- app/Traits/WalletTransactionLogs.php
+++ app/Traits/WalletTransactionLogs.php
@@ -1,24 +1,31 @@
 <?php
+
 namespace App\Traits;
 
 use App\Models\WalletLog;
-use Illuminate\Http\Request;
-use phpDocumentor\Reflection\Types\Void_;
 
-trait WalletTransactionLogs{
+trait WalletTransactionLogs
+{
     public function log_wallet_transactions($array): void
     {
         // DATA_FROM_MAIN_WALLET, DATA_FROM_DATA_WALLET, AIRTIME, CABLE, BILLS, BULK_DATA_PURCHASE
-        // ADMIN_WALLET_CREDITING, USER_WALLET_CREDITING?:check use , CRYSTALPAY_WALLET_FUNDING
-        WalletLog::create([
-            'affiliate_id' => $array['affiliate_id'],
+        // ADMIN_WALLET_CREDITING, USER_WALLET_CREDITING, CRYSTALPAY_WALLET_FUNDING
+
+        $data = [
             'user_id' => $array['user_id'],
-            'transaction_category' => $array['transaction_category'], //  
+            'transaction_category' => $array['transaction_category'],
             'balance_before' => $array['balance_before'],
             'balance_after' => $array['balance_after'],
             'description' => $array['description'],
             'action_by' => $array['action_by'],
-            'transaction_id' => $array['transaction_id'] ?? NULL,
-        ]);
+            'transaction_id' => $array['transaction_id'] ?? null,
+        ];
+
+        // Only attach affiliate_id if provided
+        if (isset($array['affiliate_id'])) {
+            $data['affiliate_id'] = $array['affiliate_id'];
+        }
+
+        WalletLog::create($data);
     }
 }

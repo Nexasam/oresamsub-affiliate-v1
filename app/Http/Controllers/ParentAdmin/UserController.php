892fec15de3ad3d806cbede8a7208529ec7e201d
Sammiee4real <adebsholey4real@gmail.com>
2026-08-13 13:51:33 +0100

    fix issue

--- app/Http/Controllers/ParentAdmin/UserController.php
+++ app/Http/Controllers/ParentAdmin/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller {
  public function index(Request $request) {
   $parent=$request->user('parent_admin')->parentBusiness; $affiliateIds=$parent->affiliates()->pluck('id');
-  $users=User::withoutGlobalScope('affiliate')->whereIn('affiliate_id',$affiliateIds)->with(['affiliate:id,name','role:id,role_name','user_plan:id,plan_name,plan_level'])->withCount('transactions')
+  $users=User::withoutGlobalScope('affiliate')->whereIn('affiliate_id',$affiliateIds)->with(['affiliate:id,name','role:id,role_name','user_plan:id,user_plan_name,plan_level'])->withCount('transactions')
    ->when($request->string('search')->isNotEmpty(),fn($q)=>$q->where(fn($q)=>$q->where('email','like','%'.$request->search.'%')->orWhere('first_name','like','%'.$request->search.'%')->orWhere('last_name','like','%'.$request->search.'%')))
    ->when($request->affiliate_id,fn($q,$id)=>$q->where('affiliate_id',$id))->latest()->paginate(50)->withQueryString();
   return view('parent-admin.users.index',['users'=>$users,'affiliates'=>$parent->affiliates()->orderBy('name')->get(['id','name'])]);
